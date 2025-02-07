3ca395b10451e73e008005c9fa62817ce3e8a7b3
anjumArnab <anjumarnab050@gmail.com>
2025-02-07 23:17:31 +0600

    google_font added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rest_api/screens/employee_screen.dart';
 
 void main() {
@@ -14,8 +15,7 @@ class RESTAPI extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Rest API",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme()
       ),
       home: const EmployeeScreen(),
     );
